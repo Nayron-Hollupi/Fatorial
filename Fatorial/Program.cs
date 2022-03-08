05124c38e1f2d822a97197cc81a9bc9280610e97
Nayron Hollupi <nhollupi@gmail.com>
2022-03-07 23:11:55 -0300

    Adicionar arquivos de projeto.

---
+++ Fatorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fatorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, resposta;
+
+            Console.WriteLine("Informe um valor: ");
+            numero = int.Parse(Console.ReadLine());
+
+            resposta = fatorial(numero);
+
+            Console.WriteLine("O Fatorial de {0} eh : {1}", numero, resposta);
+
+
+          
+        }
+
+      static int fatorial(int numero)
+        {
+           
+            return (numero <= 1) ? 1 : (numero * fatorial(numero - 1));
+        }
+       
+    }
+}
